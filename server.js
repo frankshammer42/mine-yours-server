c309a2de9d4358e2013fe8dff385a10da4b134f5
ruian2 <ruian2@illinois.edu>
2018-12-07 18:26:04 -0500

    Finished The Simple API

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var app = express();
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+var current_rotation = "rest";
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
 	res.render('index');
 });
+
+app.get('/api/rotate', function(req, res){
+	res.send(current_rotation);
+})
+
+app.post('/api/rotate', function(req, res){
+	let position_to_rotate = req.body.position_to_rotate;
+	current_rotation = position_to_rotate; 
+	res.send("OK");
+})
+
+
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
